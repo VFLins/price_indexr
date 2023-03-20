c5215f0b79659101e5ffc6de2c317d94770b5749
Vitor Ferreira Lins <vflins@live.com>
2023-03-20 09:35:17 -0300

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -5,13 +5,16 @@ from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship,
 from datetime import date, datetime
 
 def scan_products() -> list:
+    """Read products table to get pairs of Id and LastUpdate"""
     output = []
     with Session(DB_ENGINE) as ses:
         stmt = select(products)
         result = ses.execute(stmt).scalars()
 
         for i in result:
-                output.append([i.Id, i.LastUpdate])
+                output.append((i.Id, i.LastUpdate))
+    return output
+
 
 if __name__ == "__main__":
     print(scan_products())
