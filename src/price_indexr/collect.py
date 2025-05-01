3db5ec1cef431961749d47ca5717b4c3987b3ce3
Vitor F. Lins <vflins@live.com>
2025-05-01 18:32:43 -0300

    upd: google grid result url

--- src/price_indexr/collect.py
+++ src/price_indexr/collect.py
@@ -244,11 +244,12 @@ class SearchResponses:
                 Price = strip_price_str(
                     result.find("span", {"class": "lmQWe"}).get_text()
                 )
+                Store = result.find("span", {"class": "WJMUdc"}).get_text()
 
-                line["Url"] = f"https://www.google.com/search?q={Name.replace(" ", "%20")}&tbm=shop"
+                line["Url"] = f"https://www.google.com/search?q={Name.replace(" ", "%20")}%20{Store.replace(" ", "%20")}"
                 line["Name"] = Name
                 line["Date"] = self.Date
-                line["Store"] = result.find("span", {"class": "WJMUdc"}).get_text()
+                line["Store"] = Store
                 line["Price"] = Price[1]
                 line["Currency"] = Price[0]
                 line["ProductId"] = self.product.Id
